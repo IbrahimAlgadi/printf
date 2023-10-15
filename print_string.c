554e526600af8e5d16b90575aee160f9a8fd2eea
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-15 23:12:51 +0300

    betty Fixes

--- print_string.c
+++ print_string.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
-int print_string(char *string){
-  int count = 0;
+/**
+ * print_string - print string
+ * @string: Number
+ *
+ * Return: int
+ */
+int print_string(char *string)
+{
+	int count = 0;
 
-  for (int i = 0; string[i] != '\0'; i++){
-    count += _putchar(string[i]);
-  }
-  
-  return (count);
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		count += _putchar(string[i]);
+	}
+
+	return (count);
 }
--- print_unsigned.c
+++ print_unsigned.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
-int print_unsigned(unsigned int value){
-   int count = 0;
-  
-    /* First remove the last digit of number and print 
-    the remaining digits using recursion, then print
-    the last digit
+/**
+ * print_unsigned - print unsigned
+ * @value: unsigned int
+ *
+ * Return: int
  */
-    if (value/10)
-        count += print_decimal(value/10);
-  
-    count += _putchar(value%10 + '0');
+int print_unsigned(unsigned int value)
+{
+	int count = 0;
 
-    return (count);
+	if (value / 10)
+		count += print_decimal(value / 10);
+
+	count += _putchar(value % 10 + '0');
+
+	return (count);
 }
