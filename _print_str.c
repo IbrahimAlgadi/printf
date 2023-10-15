24984aaac13313f72816786dfe338b8f18e87388
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-15 13:55:57 +0300

    _print_str.c betty problem

--- _print_str.c
+++ _print_str.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
-int print_str(char *string){
-  int count = 0, i;
+/**
+ * print_str - a function used to print string
+ * @string: string pointer
+ *
+ * Return: Count
+ *
+ */
+int print_str(char *string)
+{
+	int count = 0, i;
 
-  for (i = 0; string[i] != '\0'; i++){
-    count += _putchar(string[i]);
-  }
-  
-  return (count);
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		count += _putchar(string[i]);
+	}
+
+	return (count);
 }
