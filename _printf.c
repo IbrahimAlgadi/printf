09838b6519681847a3c038dfce8a2b3942704d2f
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-15 12:42:45 +0300

    adding _printf

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include <stdarg.h>
+#include "main.h"
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+	int count = 0, i;
+
+	va_list data;
+	va_start(data, format);
+
+	for (i = 0; format[i] != '\0';)
+	{
+
+		/* count the number of characters */
+		/* print to the screen each character counted */
+		if (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] != ' ')
+		{
+
+			if (format[i + 1] == 'c')
+			{
+				count += _putchar(va_arg(data, int));
+			}
+			else
+			{
+				break;
+			}
+
+			i += 2;
+		}
+
+
+	}
+
+
+	return (count);
+}
