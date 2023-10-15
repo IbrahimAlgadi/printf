0dc04cda23ad5d3909eecd142b1935c36e00ee65
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-15 13:44:39 +0300

    Fix Betty Errors

--- _printf.c
+++ _printf.c
@@ -2,38 +2,39 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _printf - a function that produces output according to a format
+ * @format: identifier.
+ *
+ * Return: the length of the string.
+ *
+ */
 int _printf(const char *format, ...)
 {
 	int count = 0, i;
-
 	va_list data;
+
 	va_start(data, format);
 
 	for (i = 0; format[i] != '\0';)
 	{
-		/* count the number of characters */
-		/* print to the screen each character counted */
 		if (format[i] != '%')
 		{
-			count += _putchar(format[i]);
+			count += _putchar(format[i]); /* count characters */
 			i++;
-		}
-
-		/* if detected % in the string sequence then check the char after the % */
-		else if (format[i] == '%' && format[i + 1] != ' ')
+		} else if (format[i] == '%' && format[i + 1] != ' ')
 		{
 			switch (format[i + 1])
 			{
-				case 'c':
-					/* print character */
-					count += _putchar(va_arg(data, int));
+				case 'c': /* print character */
+					count += _putchar(va_arg(data,
+					int));
 					break;
-				case 's':
-					/* print str */
-					count += print_str(va_arg(data, char *));
+				case 's': /* print str */
+					count += print_str(va_arg(data,
+					char *));
 					break;
-				case '%':
-					/* print character */
+				case '%': /* print character */
 					count += _putchar('%');
 					break;
 				default:
@@ -43,9 +44,7 @@ int _printf(const char *format, ...)
 			i += 2;
 		}
 
-
 	}
-
 
 	return (count);
 }
